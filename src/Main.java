109bb130b9294e1b5b2246de3d087580fc3caa0d
chaenii25 <chaeun9325@naver.com>
2025-09-01 14:38:50 +0900

    main함수 수정

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,22 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 public class Main {
-    public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        int num = Integer.parseInt(bf.readLine());
+        int[] arr = new int[num];
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+        for (int i = 0; i < num; i++) {
+            arr[i] = Integer.parseInt(bf.readLine());
+        }
+
+        Arrays.sort(arr);
+        for (int i : arr) {
+            System.out.println(i);
         }
     }
 }
