141ad8865d4d6a3f9c174e600f4ad48f3d660a04
chaenii25 <chaeun9325@naver.com>
2025-09-03 15:16:41 +0900

    반복문 강의 필기

--- src/loop/DoWhile2.java
+++ src/loop/DoWhile2.java
@@ -5,6 +5,7 @@ public class DoWhile2 {
     public static void main(String[] args) {
         int i = 10;
 
+        // 먼저 하고 "그 다음 조건 판단"
         do {
             System.out.println("현재 숫자는:" + i);
             i++;
--- src/loop/While2_1.java
+++ src/loop/While2_1.java
@@ -4,14 +4,18 @@ public class While2_1 {
 
     public static void main(String[] args) {
         int sum = 0;
+        int i  =1;
 
-        sum = sum + 1;
+        sum = sum + i;
         System.out.println("i=" + 1 + " sum=" + sum);
+        i++;
 
-        sum = sum + 2;
+        sum = sum + i;
         System.out.println("i=" + 2 + " sum=" + sum);
+        i++;
 
-        sum = sum + 3;
+        sum = sum + i;
         System.out.println("i=" + 3 + " sum=" + sum);
+        i++;
     }
 }
