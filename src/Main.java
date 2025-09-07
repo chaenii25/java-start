36aa6801e1d7262ff83849cb48ad5fc155417ebb
chaenii25 <chaeun9325@naver.com>
2025-09-07 16:42:15 +0900

    메인함수 7수정

--- src/Main.java
+++ src/Main.java
@@ -7,34 +7,42 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int num = Integer.parseInt(br.readLine());
-        List<Person> people = new ArrayList<>();
+        String[] str =  br.readLine().split(" ");
+        int num = Integer.parseInt(str[0]);
+        int length = Integer.parseInt(str[1]);
+
+        Map<String, Integer> map = new HashMap<>();
 
         for (int i = 0; i < num; i++) {
-            String[] arr = br.readLine().split(" ");
-            int age = Integer.parseInt(arr[0]);
-            String name = arr[1];
+            String word = br.readLine();
 
-            people.add(new Person(age, name));
+            if (word.length() < length) continue;
+
+            if (map.containsKey(word)) {
+                map.put(word, map.get(word) + 1);
+            } else {
+                map.put(word, 1);
+            }
         }
 
-        Collections.sort(people, (a, b) -> {
-            return Integer.compare(a.age, b.age);
+        List<String> list = new ArrayList<>(map.keySet());
+
+        Collections.sort(list, (a, b) -> {
+            if (!map.get(a).equals(map.get(b))) {
+                return Integer.compare(map.get(b), map.get(a));
+            }
+
+            if (a.length() != b.length()) {
+                return Integer.compare(b.length(), a.length());
+            }
+
+            return a.compareTo(b);
         });
 
-        for (Person person : people) {
-            System.out.println(person.age + " " + person.name);
+        StringBuilder sb = new StringBuilder();
+        for (String s : list) {
+            sb.append(s).append("\n");
         }
-       
-    }
-}
-
-class Person {
-    int age;
-    String name;
-
-    Person(int age, String name) {
-        this.age = age;
-        this.name = name;
+        System.out.println(sb.toString());
     }
 }
